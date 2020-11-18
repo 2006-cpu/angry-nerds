dd08ac368e9ce999bd84c14cc98f4fe2cc79f2a3
Trinidad Padilla <tpadilla101@live.com>
2020-11-17 22:17:07 -0800

    finished the order_products table

--- db/init_db.js
+++ db/init_db.js
@@ -4,12 +4,37 @@ const {
   // other db methods 
 } = require('./index');
 
+async function dropTables() {
+  console.log('Dropping All Tables...');
+
+  try {
+    client.query(`
+    DROP TABLE IF EXISTS orders;
+    DROP TABLE IF EXISTS order__products;
+    DROP TABLE IF EXISTS users;
+    DROP TABLE IF EXISTS products;
+    `)
+  } catch(error) {
+    console.log('Error Dropping Tables');
+  }
+}
+
 async function buildTables() {
+  console.log('Starting to build tables...');
+
   try {
     client.connect();
+    `CREATE TABLE order_products(
+      id SERIAL PRIMARY KEY,
+      "productId" INTEGER REFERENCES products(id),
+      "orderId" INTEGER REFERENCES orders(id),
+      price INTEGER NOT NULL,
+      quantity INTEGER NOT NULL DEFAULT (0)
+
+    )`
 
     // drop tables in correct order
-
+    
     // build tables in correct order
 
   } catch (error) {
