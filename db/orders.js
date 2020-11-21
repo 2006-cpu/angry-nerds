845233c58dd83c33a4e9c9571741d211d971f5ad
Trinidad Padilla <tpadilla101@live.com>
2020-11-21 13:33:12 -0800

    finished orders db adapters

--- db/orders.js
+++ db/orders.js
@@ -29,24 +29,78 @@ const {client} = require("./index")
    
 /* THIS IS FOR THE getOrdersByUser ADAPTER */
 
-async function getOrdersByUser({ username }) {
-    try {
-        const { rows: order }  = await client.query(`
-        SELECT orders.*, users.id
-        AS "userId"
-        JOIN users ON users.id = orders."userId"
-        WHERE users.id = $1
-        `, [username]);
+    async function getOrdersByUser({ username }) {
+        try {
+            const { rows: order }  = await client.query(`
+            SELECT orders.*, users.id
+            AS "userId"
+            JOIN users ON users.id = orders."userId"
+            WHERE users.id = $1
+            `, [username]);
 
-        return order;
-    } catch (error) {
-        throw error;
+            return order;
+        } catch (error) {
+            throw error;
+        }
     }
-}
+
+/* THIS IS FOR THE getOrdersByProductId ADAPTER */
+    async function getOrdersByProductId({ id }) {
+        try {
+            const { rows: order } = await client.query(`
+            SELECT orders.*
+            FROM orders
+            JOIN order_products ON
+            order_products."orderId" = 
+            orders.id
+            WHERE order_products."productId" = $1
+            `, [ id ])
+
+            return order;
+
+        } catch (error) {
+            throw error;
+        }
+    }
+
+/* THIS IS FOR THE getCartByUser ADAPTER */
+    async function getCartByUser({id}) {
+        try {
+            const { rows: [ order ] } = await
+            client.query(`
+            SELECT orders.*
+            WHERE orders.status = "created"
+            WHERE order."userId" = $1
+            `, [ id ])
+
+            return order;
+        } catch (error) {
+            throw error;
+        }
+    }
+
+/* THIS IS FOR THE createOrder ADAPTER */
+    async function createOrder({ status, userId }) {
+        try {
+            const { rows: [ order ] } = await client.query(`
+                INSERT INTO orders
+                (status, "userId")
+                VALUES($1, $2)
+                RETURNING *;
+            `, [status, userId]);
+
+            return order;
+        } catch (error) {
+            throw error;
+        }
+    }
 
 module.exports = {
     getOrderById,
     getAllOrders,
-    getOrdersByUser
+    getOrdersByUser,
+    getOrdersByProductId,
+    getCartByUser,
+    createOrder
 }
 
