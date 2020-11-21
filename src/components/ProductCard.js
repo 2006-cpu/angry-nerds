18fcaa7c06cf37cd674b688071b23fc19d6b086c
Katiana Camacho-Villalon <katianajanea@gmail.com>
2020-11-21 01:37:54 -0800

    chore(single-product-route-placeholder): Add uninterpolated links

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -4,15 +4,17 @@ import ListGroup from 'react-bootstrap/ListGroup'
 import ListGroupItem from 'react-bootstrap/ListGroupItem'
 import Button from 'react-bootstrap/Button'
 
+import {Link, useParams} from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Prod = (props) => {
     const {id, name, description, price, imageURL, inStock, category} = props
+    
     return <Card style={{ width: '18rem', margin: '1rem', 
     boxShadow: '0 6px 10px -5px' }} onClick={() => {console.log('selected product with id ', id)}}>
     <Card.Img variant="top" src={imageURL} />
     <Card.Body>
-<Card.Title>{name}</Card.Title>
+<Card.Title><Link to="/products/:productId">{name}</Link></Card.Title>
       <Card.Text>
         {description}
       </Card.Text>
