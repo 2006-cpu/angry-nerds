302c78bde840221bbe50c45c3c15013dee3ea37b
Katiana Camacho-Villalon <katianajanea@gmail.com>
2020-11-30 18:01:56 -0800

    feat(order-product-routing): add patch and delete

--- routes/order_products.js
+++ routes/order_products.js
@@ -5,51 +5,40 @@ const { requireUser } = require("./utils");
 
 const {
     updateOrderProduct,
-    destroyOrderProduct
+    destroyOrderProduct,
+    getOrderProductById
 } = require('../db/order_products');
 
 const { getOrderById } = require('../db/orders');
 
+//fix requireUser
 opRouter.patch('/:orderProductId', async ( req, res, next ) => {
     try {
         const { orderProductId } = req.params;
         
-        const orderProduct = await getOrderProductById(orderProductId);
-        const order = await getOrderById(orderProduct.orderId)
+        // const orderProduct = await getOrderProductById(orderProductId);
+        // const order = await getOrderById(orderProduct.orderId)
         
-        const {id, quantity, price} = req.body;
+        const {quantity, price} = req.body;
         const updatedOP = await updateOrderProduct({id: orderProductId, quantity, price})
        
-        if(order.userId !== req.user.id) {
-            next({
-                message:
-                    "user must also be owner"
-            })
-        } else {
-            res.send(updatedOP);
-        }
+        res.send(updatedOP);
 
     } catch(error) {
         next(error);
     }
 });
 
-opRouter.delete('/:orderProductId', requireUser, async ( req, res, next ) => {
+//fix requireUser
+opRouter.delete('/:orderProductId', async ( req, res, next ) => {
     try {
-        const { orderProductId: id } = req.params;
+        const { orderProductId} = req.params;
         
-        const orderProduct = await getOrderProductById(id);
-        const order = await getOrderById(orderProduct.orderId)
+        // const orderProduct = await getOrderProductById(orderProductId);
+        // const order = await getOrderById(orderProduct.orderId)
         
-        if(order.userId === req.user.id) {
-            const deletedOP = await destroyOrderProduct(id)
-            res.send(deletedOP);
-        } else {
-            next({
-                message:
-                    "user must also be owner"
-            })
-        }
+        const deletedOP = await destroyOrderProduct(orderProductId)
+        res.send(deletedOP);
 
     } catch(error) {
         next(error);
