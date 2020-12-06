7fd087d18aa6fb653cec5ce23d68296d36a030ab
Trinidad Padilla <tpadilla101@live.com>
2020-12-05 22:38:23 -0800

    working on admin adapters and routes

---
+++ db/admin.js
@@ -0,0 +1,78 @@
+const {client} = require("./index")
+const { getOrderById } = require('../db/orders');
+
+/* THIS IS FOR THE updateProduct ADAPTER */
+/* async function updateProduct(product) {
+    const {name, description, price, inStock, imageURL, category} = product;
+
+    try {
+        const { rows: [product] } = await client.query(`
+            UPDATE products
+            SET name = $2, description = $3, price = $4, inStock = $5, imageURL = $6, category = $7
+            WHERE id = $1
+            RETURNING *;
+        `, [name, description, price, inStock, imageURL, category])
+
+        return product;
+    } catch (error) {
+        throw error;
+    }
+} */
+
+/* THIS IS FOR THE destroyProduct ADAPTER */
+async function destroyProduct({id}) {
+    try {
+        const order = await getOrderById(id);
+        if(order.status === 
+            'completed'){
+                console.log("The order has a status of completed")
+                return;
+            }
+        await client.query(`
+        DELETE from order_products
+        WHERE "productId"=$1
+        RETURNING *;
+        `, [id]);
+
+        const { rows: [product] } = await client.query (`
+        DELETE from products
+        WHERE id=$1
+        RETURNING *;
+        `, [id]);
+        return product;
+    } catch (error) {
+        throw error;
+    }
+};
+
+/* THIS IS FOR THE updateUser ADAPTER */
+const updateUser = async ({id, ...fields})=>{
+    const setString = Object.keys(fields).map(
+        (key, index) => `"${ key }"=$${ index + 1}`
+    ).join(', ');
+    
+    const objVal = Object.values(fields)
+    if( setString.length === 0){
+        return;
+    }
+    
+    objVal.push(id);
+
+    try {
+        const {rows: [user]} = await client.query(`
+            UPDATE users
+            SET ${setString}
+            WHERE id = $${objVal.length}
+            RETURNING *;
+        `, objVal);
+        return user;
+    } catch (error) {
+        throw error;
+    }
+};
+
+module.exports = {
+    destroyProduct,
+    updateProduct,
+    updateUser
+}
--- routes/products.js
+++ routes/products.js
@@ -8,6 +8,10 @@ const {
     createProduct
 } = require('../db/products')
 
+const {
+    destroyProduct
+} = require('../db/admin');
+
 /* ------------------------------------------------------------ */
 /* THIS IS THE GET/products ROUTER */
 
@@ -54,5 +58,58 @@ const {
             next(error);
         }
     });
+
+/* ------------------------------------------------------------ */
+/* THIS IS THE GET /products/:productId/orders ROUTER */
+
+productsRouter.get('/:productId/orders', async (req,res,next) => {
+    try {
+      const {productId} = req.params;
+      const prefix = 'Bearer ';
+      const auth = req.header('Authorization');
+      if (auth.startsWith(prefix)) {
+      const token = auth.slice(prefix.length);
+      if (token){
+      const { id } = jwt.verify(token, JWT_SECRET);
+      const user = await getUserById(id);
+        if (id && user.isAdmin===true) {
+          const orders = await getOrdersByProduct({id})
+          res.send(orders)
+        } else {
+          res.send({message:'You must be an admin to view these orders'})
+          }
+      }
+    }
+    } catch (error) {
+      console.log(error);
+      next(error);
+    }
+});
+
+/* ------------------------------------------------------------ */
+/* THIS IS THE DELETE/products/:productId ROUTER for admins only */
+productsRouter.delete('/:productId', async (req,res,next) => {
+    try {
+      const {productId} = req.params;
+      const prefix = 'Bearer ';
+      const auth = req.header('Authorization');
+      if (auth.startsWith(prefix)) {
+      const token = auth.slice(prefix.length);
+      if (token){
+      const { id } = jwt.verify(token, JWT_SECRET);
+      const user = await getUserById(id);
+        if (id && user.isAdmin===true) {
+          const deletedProduct = await destroyProduct({id})
+          res.send(deletedProduct)
+        } else {
+          res.send({message:'Only admins can delete products'})
+          }
+      }
+    }
+    } catch (error) {
+      console.log(error);
+      next(error);
+    }
+});
 
 module.exports = productsRouter;
