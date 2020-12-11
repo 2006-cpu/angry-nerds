a3e148c535b28c0f3ff81390f393b6668ee7e411
Nrlopez16 <nrlopez16@hotmail.com>
2020-12-10 23:13:50 -0800

    feat(frontend):Products in cart cannot be readded

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,13 +2,14 @@ import React, { useState, useEffect } from 'react';
 import Carousel from 'react-bootstrap/Carousel'
 
 import {getAllProducts} from '../api'
-
+import {getCurrentCart} from '../auth'
 import {Prod, Footer} from './index'
 
 const HomePage = (props) => {
     const {setFetchId} = props
     const [productRender, setProductRender] = useState([])
     const [selectedId, setSelectedId] = useState('')
+    const [inCart, setInCart] = useState(getCurrentCart())
 
     useEffect(() => {
         async function fetchProducts(){
@@ -28,7 +29,7 @@ const HomePage = (props) => {
     return <div>
       <div>
         <Carousel>
-          <Carousel.Item>
+          <Carousel.Item style={{width: '100%', maxHeight: '34rem'}}>
             <img
               className="d-block w-100"
               src="https://store-images.s-microsoft.com/image/apps.3780.14600436456605939.002f21b5-1ec5-42de-bb50-ad2d62814822.0e7d2807-7ad8-43bd-9039-112149dc5529"
@@ -39,7 +40,7 @@ const HomePage = (props) => {
               <p style={{textShadow: '1px 1px 2px #000'}}>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
             </Carousel.Caption>
           </Carousel.Item>
-          <Carousel.Item>
+          <Carousel.Item style={{width: '100%', maxHeight: '34rem'}}>
             <img
               className="d-block w-100"
               src="https://i.pinimg.com/originals/de/65/00/de6500daee98e24fcf5c22c9cf15c03e.jpg"
@@ -51,7 +52,7 @@ const HomePage = (props) => {
               <p style={{textShadow: '1px 1px 2px #000'}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
             </Carousel.Caption>
           </Carousel.Item>
-          <Carousel.Item>
+          <Carousel.Item style={{width: '100%', maxHeight: '34rem'}}>
             <img
               className="d-block w-100"
               src="https://images.wallpaperscraft.com/image/microphone_metal_device_149328_1920x1080.jpg"
@@ -70,9 +71,17 @@ const HomePage = (props) => {
       <div style={{ backgroundColor: 'lightgray', borderTop: '1px solid black',
       display: 'flex', flexDirection: 'row', flexShrink: '0', overflowX: 'scroll', width: '100%', height: '100%'}}>
       {productRender.map((product) => {
-          return <div style={{width: '18rem', margin: '.2rem'}}>
-                <Prod key={product.id} product={product} setSelectedId={setSelectedId} />
-              </div>})}
+  const added = inCart.some(cartProd => cartProd.id === product.id)
+  console.log(' product with id ',product.id, 'bool ', added )
+  if(added){
+    return <div style={{width: '18rem', margin: '.2rem'}}>
+    <Prod key={product.id} product={product} setSelectedId={setSelectedId}inCart={true} />
+  </div>}
+  else if(!added){
+    return <div style={{width: '18rem', margin: '.2rem'}}>
+    <Prod key={product.id} product={product} setSelectedId={setSelectedId}inCart={false} />
+  </div>}
+})}
       </div>
 <Footer />
       </div>
